3914858194f8a64d15c70310c53e02364661df94
abc-26 <159798643+abc-26@users.noreply.github.com>
2025-02-12 20:42:50 +0530

    Solution to Set nth Bit of a Number

--- Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
+++ Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 int main() {
     int num, n;
-
-    // Input the number and the position of the bit to be set
+// Input the number and the position of the bit to be set
     scanf("%d", &num);
     scanf("%d", &n);
+// Set the nth bit to 1 using bitwise OR
+     num = num | (1 << n);
 
-    // Set the nth bit to 1 using bitwise OR
-    num = num | (1 << n);
-
-    // Print the decimal result
-    
+    // Print the decimal result (the updated number)
+    printf("%d\n", num);
 
     // Print the binary representation without leading zeros
     int printed = 0;  // Flag to track whether we've printed the first 1 bit
@@ -19,6 +17,7 @@ int main() {
     for (int i = 31; i >= 0; i--) {
         int bit = (num >> i) & 1;
         
+        // Start printing once we hit the first 1 bit
         if (bit == 1) {
             printed = 1;
         }
@@ -29,7 +28,7 @@ int main() {
         }
     }
 
-    // If no bits were printed, print 0 for the case of num being 0
+    // If no bits were printed (num is 0), print 0 for the case of num being 0
     if (!printed) {
         printf("0");
     }
